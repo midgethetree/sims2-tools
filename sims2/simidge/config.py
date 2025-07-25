e64aa3e11e81072ca8c7f6a4dfb38371750755ea
midgethetree <76988393+midgethetree@users.noreply.github.com>
2025-07-25 06:44:21 -0400

    refactor(simidge.config): use ConfigParser.read_dict for config defaults

--- sims2/simidge/config.py
+++ sims2/simidge/config.py
@@ -12,14 +12,20 @@ def _load_config() -> None:
         config.read(path)
         return
 
-    config["paths"] = {}
-    config["paths"]["downloads"] = str(
-        Path.home() / "Documents/EA Games/The Sims™ 2 Ultimate Collection/Downloads",
-    )
-    config["paths"]["objects"] = str(
-        Path(
-            "C:/Program Files (x86)/Origin Games/The Sims 2 Ultimate Collection/Fun with Pets/SP9/TSData/Res/Objects/objects.package",
-        ),
+    config.read_dict(
+        {
+            "paths": {
+                "downloads": str(
+                    Path.home()
+                    / "Documents/EA Games/The Sims™ 2 Ultimate Collection/Downloads",
+                ),
+                "objects": str(
+                    Path(
+                        "C:/Program Files (x86)/Origin Games/The Sims 2 Ultimate Collection/Fun with Pets/SP9/TSData/Res/Objects/objects.package",
+                    ),
+                ),
+            },
+        },
     )
     path.parent.mkdir(parents=True, exist_ok=True)
     with path.open("w", encoding="utf-8") as file:
