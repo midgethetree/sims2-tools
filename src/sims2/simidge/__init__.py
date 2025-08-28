d436a145d9125dd216fd4cf89a901fad6dc81da2
midgethetree <76988393+midgethetree@users.noreply.github.com>
2025-08-28 02:10:54 -0400

    refactor: import __main__.main in simidge and simtracker

--- src/sims2/simidge/__init__.py
+++ src/sims2/simidge/__init__.py
@@ -1,1 +1,7 @@
 """A tool for searching in .package files."""
+
+from .__main__ import main
+
+__all__ = [
+    "main",
+]
--- src/sims2/simtracker/__init__.py
+++ src/sims2/simtracker/__init__.py
@@ -1,1 +1,7 @@
 """A program for viewing information about your neighborhoods in a tabulated spreadsheet."""
+
+from .__main__ import main
+
+__all__ = [
+    "main",
+]
